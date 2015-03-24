617aae549d8eb56ec2e46c98a59f3a999a4d441d
HugoA <alberto.hugo05@gmail.com>
2015-03-24 16:04:56 +0100

    First Commit

---
+++ SKSpriteDemo/MenuScene.h
@@ -0,0 +1,13 @@
+//
+//  MenuScene.h
+//  SKSpriteDemo
+//
+//  Created by Hugo Alberto on 24/03/2015.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MenuScene : SKScene
+
+@end
---
+++ SKSpriteDemo/MenuScene.m
@@ -0,0 +1,13 @@
+//
+//  MenuScene.m
+//  SKSpriteDemo
+//
+//  Created by Hugo Alberto on 24/03/2015.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+#import "MenuScene.h"
+
+@implementation MenuScene
+
+@end
